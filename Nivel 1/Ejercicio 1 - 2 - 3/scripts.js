e2139ab65417a85146ffbd2ec5f3982f33274736
Patri <witty.free@gmail.com>
2021-03-23 09:30:30 +0100

    ejercicios

---
+++ Nivel 1/Ejercicio 1 - 2 - 3/scripts.js
@@ -0,0 +1,11 @@
+//EJERCICIO 1
+console.log("Hola Mundo");
+
+//EJERCICIO 2
+alert ("Me llamo Patricia");
+
+//EJERCICIO 3
+let nombre = "Patricia";
+let apellido = "Garcia";
+console.log(nombre +" "+ apellido);
+
---
+++ Nivel 1/Ejercicio 4 - 5/scripts.js
@@ -0,0 +1,15 @@
+//EJERCICIO 4
+let numero1 = 44;
+let numero2 = 20;
+let resultado = numero1 + numero2;
+console.log(`La suma entre ${numero1} y ${numero2} es ${resultado}`);
+
+//EJERCICIO 5
+
+let nota_examen = prompt("Inserta un número entre 1 y 10");
+if(nota_examen>=5){
+    alert("¡Bien! Has aprobado el examen");
+}else{
+    alert("Ooooh! No has aprobado el examen");
+}
+
