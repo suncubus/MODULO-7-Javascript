a6e358a8309cb38cd925281c4318bd1607b893d5
Patri <witty.free@gmail.com>
2021-04-05 13:06:44 +0200

    Add files via upload

---
+++ 01 Ejercicios Básicos/Nivel 1/Ejercicio 6 - 7/scripts.js
@@ -0,0 +1,43 @@
+//EJERCICIO 6
+let frase = "Tengo un coche de color azul";
+
+//reemplazar verde por azul
+function mostrarColor(){
+    let res1 = frase.replace("azul", "verde");
+    document.getElementById("resultado1").innerHTML =   res1;
+}
+
+//reemplazar o por u
+function mostrarVocales(){
+    let res2 = "";
+    let i;
+    for(i=0;i<frase.length;i++){
+        if(frase[i] == "o"){
+            res2 += "u";
+        }else{
+            res2 += frase[i];
+        }        
+    }
+    resultado2.innerHTML = res2; 
+}
+
+function mostrarVocales2(){
+    let res4 = frase.replace(/o/g,"u");
+    resultado4.innerHTML = res4; 
+}
+//EJERCICIO 7
+let lista = ["mesa", "silla", "ordenador", "libreta"];
+
+function mostrarListado(){
+    let i;
+    let res3 = "";
+    for(i=0;i<lista.length;i++){
+        res3 += `El objeto ${lista[i]} está en la posición ${i} <br>`;
+    }
+    resultado3.innerHTML = res3; 
+}
+
+
+
+
+
